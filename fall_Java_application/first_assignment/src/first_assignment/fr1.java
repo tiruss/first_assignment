7bbb945b183db6e86491eeb52d0366798b13dd0d
DongGoo Kang <tiruss@naver.com>
2017-09-05 13:02:14 +0900

    Add files via upload

---
+++ fall_Java_application/first_assignment/src/first_assignment/fr1.java
@@ -0,0 +1,13 @@
+package first_assignment;
+
+public class fr1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2012203001");
+		System.out.println("°­µ¿±¸");
+		System.out.println("010-5644-6018");
+		System.out.println("tiruss");
+	}
+
+}
